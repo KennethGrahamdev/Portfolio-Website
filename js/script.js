44f96ddcfe51c3a7c909a051aed7ef8031e1e4fc
kglbfinest <113145655+kglbfinest@users.noreply.github.com>
2023-03-17 15:09:52 -0700

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,32 @@
+///////////////////////////////////////////////////////////
+// Set current year
+const yearEL = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+console.log("currentYear");
+yearEL.textContent = currentYear;
+///////////////////////////////////////////////////////////
+// Sticky Navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+///////////////////////////////////////////////////////////

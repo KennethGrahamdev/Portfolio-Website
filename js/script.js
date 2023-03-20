56259ed73ca4ec4e91fcc0a0d82fca4d232c48dc
kglbfinest <113145655+kglbfinest@users.noreply.github.com>
2023-03-20 15:39:40 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -30,3 +30,42 @@ const obs = new IntersectionObserver(
 );
 obs.observe(sectionHeroEl);
 ///////////////////////////////////////////////////////////
+// Smooth Scroll
+
+// function smoothScroll(target,duration){
+//     var target = document.querySelector(target);
+//     var targetPosition = target.getBoundingClientRect().top;
+//     var startPosition = window.pageYOffset;
+//     var distance = targetPosition - startPosition;
+//     var startTime = null;
+
+//     function animation(currentTime){
+//         if(startTime === null) startTime = currentTime;
+//         var timeElapsed = currentTime - startTime;
+//         var run = ease(timeElapsed,startPosition,distance,duration);
+//         window.scrollTo(0,run);
+//         if(timeElapsed < duration) requestAnimationFrame(animation);
+//     }
+
+//     function ease(t, b, c, d) {
+//         t /= d / 2;
+//         if (t < 1) return c / 2 * t * t + b;
+//         t--;
+//         return -c / 2 * (t* (t - 2) - 1) + b;
+
+//     }
+
+//     requestAnimationFrame(animation);
+
+// }
+
+// var header = document.querySelector('.header')
+// header.addEventListener('click',function(){
+//     smoothScroll('.about-section', 1000);
+// })
+
+// var about_section = document.querySelector('.about-section')
+// about_section.addEventListener('click',function(){
+//     smoothScroll('.portfolio-section', 1000);
+// })
+///////////////////////////////////////////////////////////

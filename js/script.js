37ed380867fc4ffb09ad3c25680c4aceb26f9e28
kglbfinest <113145655+kglbfinest@users.noreply.github.com>
2023-03-24 13:28:48 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,71 +1,47 @@
 ///////////////////////////////////////////////////////////
-// Set current year
+// Highlights what section the user is viewing on the navigation bar.
+const sections = document.querySelectorAll('section');
+const navLi = document.querySelectorAll('nav ul li a');
+
+window.addEventListener('scroll', ()=> {
+  let current = '';
+  sections.forEach( section => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if(scrollY > (sectionTop - sectionHeight/6)) {
+      current = section.getAttribute('id')
+    }
+  })
+  navLi.forEach( a => {
+    a.classList.remove('active');
+    if(a.classList.contains(current)){
+      a.classList.add('active')
+    }
+  })
+})
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
+// Lets the user know their message has successfully been sent.
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyI1GTO-MyBNlpQqYni93LABLzoZ3cqxfj8LXexF1pfc2SScYX9m4CaJhbKGZoebtWf/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {msg.innerHTML = "Message Sent"
+    setTimeout(function(){
+      msg.innerHTML = ""
+    },5000)
+    form.reset()
+  })
+    .catch(error => console.error('Error!', error.message))
+})
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
+// Sets the current year in the footer section.
 const yearEL = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 console.log("currentYear");
 yearEL.textContent = currentYear;
-///////////////////////////////////////////////////////////
-// Sticky Navigation
-const sectionHeroEl = document.querySelector(".section-hero");
-
-const obs = new IntersectionObserver(
-  function (entries) {
-    const ent = entries[0];
-    console.log(ent);
-
-    if (ent.isIntersecting === false) {
-      document.body.classList.add("sticky");
-    }
-
-    if (ent.isIntersecting === true) {
-      document.body.classList.remove("sticky");
-    }
-  },
-  {
-    // In the viewport
-    root: null,
-    threshold: 0,
-    rootMargin: "-80px",
-  }
-);
-obs.observe(sectionHeroEl);
-///////////////////////////////////////////////////////////
-// Smooth Scroll
-
-// function smoothScroll(target,duration){
-//     var target = document.querySelector(target);
-//     var targetPosition = target.getBoundingClientRect().top;
-//     var startPosition = window.pageYOffset;
-//     var distance = targetPosition - startPosition;
-//     var startTime = null;
-
-//     function animation(currentTime){
-//         if(startTime === null) startTime = currentTime;
-//         var timeElapsed = currentTime - startTime;
-//         var run = ease(timeElapsed,startPosition,distance,duration);
-//         window.scrollTo(0,run);
-//         if(timeElapsed < duration) requestAnimationFrame(animation);
-//     }
-
-//     function ease(t, b, c, d) {
-//         t /= d / 2;
-//         if (t < 1) return c / 2 * t * t + b;
-//         t--;
-//         return -c / 2 * (t* (t - 2) - 1) + b;
-
-//     }
-
-//     requestAnimationFrame(animation);
-
-// }
-
-// var header = document.querySelector('.header')
-// header.addEventListener('click',function(){
-//     smoothScroll('.about-section', 1000);
-// })
-
-// var about_section = document.querySelector('.about-section')
-// about_section.addEventListener('click',function(){
-//     smoothScroll('.portfolio-section', 1000);
-// })
 ///////////////////////////////////////////////////////////
